591252e45384c21c10a57f99c2a170a8ec01fb7a
efako <edvinfako@gmail.com>
2025-09-22 16:30:41 +0200

    setup

---
+++ src/cft/__init__.py
@@ -0,0 +1,17 @@
+"""
+CFT
+~~~~~~~~~~
+
+CFT - stands for Covalent Field Theory. This package provides functionality for working with 2D manifolds for interfaces.
+"""
+
+__version__ = "0.1.0"
+
+# Optional: import key components for easy access
+#from .core import main_function
+#from .utils import helper_function
+#
+#__all__ = [
+#    "main_function",
+#    "helper_function",
+#]
