1990b3ec9d64f54827f5931440a5b1f3d548c728
efako <edvinfako@gmail.com>
2025-09-23 23:49:57 +0200

    test scripts

---
+++ notebooks/coverage_test.py
@@ -0,0 +1,39 @@
+from autoadsorbate import Surface, Fragment
+from ase.io import read, write
+from cft import Manifold
+from cft.mesh_utils import estimate_radius_decay
+from ase import Atoms
+import numpy as np
+from ase.visualize import view
+import matplotlib.pyplot as plt
+
+from mace.calculators import mace_mp
+calc = mace_mp(model=
+               '/mnt/c/Users/ef/Desktop/tmp/mace-mh-nl-pbe.model',
+               device='cpu',
+               head='matpes_r2scan')
+
+atoms = read('./test_particle.xyz')
+print(f'{len(atoms) = }')
+
+m = Manifold(
+    atoms,
+    precision = 1.,
+    mode = 'particle',
+    touch_sphere_size = 3.5,
+    calc = calc
+    )
+
+f = Fragment('Cl[PH+](CC(C)C)(CC(C)C)', to_initialize=1, prune_rms_thresh=.0001)
+
+m.make_fragment_population(
+    population_size = 30,
+    fragment = f,
+    coverage = .8
+    )
+
+m.evaluate_surf_population()
+
+write('surf_pop.xyz', m.surf_population)
+plt.plot([a.info['static_energy'] for a in m.surf_population])
+plt.show()
---
+++ notebooks/probe_test.py
@@ -0,0 +1,41 @@
+from autoadsorbate import Surface, Fragment
+from ase.io import read, write
+from cft import Manifold
+from ase import Atoms
+import numpy as np
+from ase.visualize import view
+
+from mace.calculators import mace_mp
+calc = mace_mp(model=
+               '/mnt/c/Users/ef/Desktop/tmp/mace-mh-nl-pbe.model',
+               device='cpu',
+               head='matpes_r2scan')
+
+atoms = read('./test_particle.xyz')
+atoms.positions -= np.array([10,10,10])
+atoms = atoms[np.linalg.norm(atoms.positions, axis=1) < 4.]
+atoms.positions += np.array([10,10,10])
+
+# atoms = read('./test_slab.xyz')
+print(f'{len(atoms) = }')
+
+m = Manifold(
+    atoms,
+    precision = 10.,
+    mode = 'particle',
+    touch_sphere_size = 2.5
+    # calc = calc
+    )
+
+# f = Fragment('ClC(=O)[O-]', to_initialize=2, prune_rms_thresh=.0001)
+# f = Fragment('ClC', to_initialize=1, prune_rms_thresh=.0001)
+f = Fragment('Cl[PH+](CC(C)C)(CC(C)C)', to_initialize=1, prune_rms_thresh=.0001)
+
+# view(f.get_conformer(0))
+
+m.calc = calc
+probes = [f]#Atoms(['H'], [[0,0,0]])
+m.run_probe_scan(probes)
+
+m.write_grid('grd.xyz', inclde_atoms=False)
+
